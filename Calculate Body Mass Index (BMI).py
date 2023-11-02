71adf82a18a387f7b5bffd39060573831fe72a2d
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-11-02 13:00:50 -0700

    Add files via upload

---
+++ Calculate Body Mass Index (BMI).py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+weight = float(input("Enter weight (kilo): "))
+height = float(input("Enter height (meter): "))
+
+
+# In[9]:
+
+
+bmi = weight/(height ** 2)
+
+
+# In[10]:
+
+
+print(f"Body Mas Index (BMI) is: {bmi:.2f}")
+
+
+# In[11]:
+
+
+if bmi < 18.5:
+    print("Underweight")
+elif 18.5 <= bmi < 24.9:
+    print("Healthy Weight Range")
+elif 25 <= bmi < 29.9:
+    print("Overweight")
+else:
+    print("OBESE")
+
+
+# In[ ]:
+
+
+
+
