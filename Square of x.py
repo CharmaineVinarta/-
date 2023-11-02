ce4b36ceb78f95fe01c6036524acfbd559d9d94d
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-11-02 13:44:17 -0700

    Add files via upload

---
+++ Square of x.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Define the variable x
+x = 4
+
+
+# In[3]:
+
+
+# Calculate the cube of x
+cube = x ** 3
+
+
+# In[5]:
+
+
+# Display the result
+print(f"The cube of {x} is {cube}")
+
+
+# In[ ]:
+
+
+
+
