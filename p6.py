9221cde6a3371e55437cbdd431cd59ff1ac1489e
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-11-10 15:55:47 -0800

    Create p6.py

---
+++ p6.py
@@ -0,0 +1,28 @@
+# Complete the function solveMeFirst to compute the sum of two integers.
+Example 
+a = 3
+b = 7
+Return 10
+
+Function Description
+Complete the solveMeFirst function in the editor below.
+solveMeFirst has the following parameters:
+int a: the first value
+int b: the second value
+Returns 
+- int: the sum of a and b
+
+
+def solveMeFirst(a, b):
+    # Return the sum of a and b
+    return a + b
+
+# Take input for a and b
+a = int(input())
+b = int(input())
+
+# Call the solveMeFirst function and store the result in 'res'
+res = solveMeFirst(a, b)
+
+# Print the result
+print(res)
