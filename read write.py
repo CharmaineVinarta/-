eee326e0cf57bf13c835ed77a6f80ca8f48d146d
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-11-16 16:50:48 -0800

    Update read write.py

--- read write.py
+++ read write.py
@@ -1,5 +1,6 @@
+# reading file
 # always starts with 'with', then open function
-
+# Option 1 to open
 with open("file_test", "r") as f:
     print(f.read())
 
@@ -13,8 +14,18 @@ with open("file_test", "r") as f:
     for line in f:
         print(line.rstrip("\n"))
 
-  
+# other option to open
 # if file i snot in the same folder do this, but double the backslash
 with open("C:\\Users\\cemvi\\PycharmProjects\\pythonProject\\file_test", "r") as f:
     for line in f:
         print(line.rstrip("\n"))
+
+
+# Writing file
+# writing a file, if re-writing the old content will be deleted
+with open("new_file", "w") as f:
+    f.write("Hello World")
+
+# if want to add, use mode a as append
+with open("new_file", "a") as f:
+    f.write(" adding a new word, Hello World")
